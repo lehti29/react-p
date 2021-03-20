ee2d7545621d30c3ea779c5a183785b29b408427
Emil Lehti <29.lehti@gmail.com>
2021-03-20 15:14:01 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hi,
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <p>My name is Emil</p>
+      <span className="info">I'm a frontend developer and UX-designer from Stockholm, Sweden</span>
     </div>
   );
 }
